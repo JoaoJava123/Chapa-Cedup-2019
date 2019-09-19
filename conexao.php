06b2d8dbd42e16e5fee83b450297fa52f852c212
AlhoQueimado <46958386+AlhoQueimado@users.noreply.github.com>
2019-09-19 10:43:31 -0300

    Create conexao.php

---
+++ conexao.php
@@ -0,0 +1,10 @@
+<?php
+$conexao = mysqli_connect("127.0.0.1", "root", "", "urna");
+
+if (!$conexao) {
+    echo "Error: Unable to connect to MySQL." . PHP_EOL;
+    echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
+    echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
+    exit;
+}
+?>
